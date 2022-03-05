16c3528c4f462d4e148badd9a7bc66ce952ae88b
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-03-06 00:22:40 +0800

    Reverted recent changes

--- src/js/controller.js
+++ src/js/controller.js
@@ -137,6 +137,5 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Branch 🌲🌲🌲');
 };
 init();
